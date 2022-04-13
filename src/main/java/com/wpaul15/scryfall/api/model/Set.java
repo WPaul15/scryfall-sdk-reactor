cd8e4d462b2e9f3ce84c58c049a99159370cfa00
WPaul15 <willem.m.paul@gmail.com>
2022-04-12 21:42:57 -0500

    Add set model classes

---
+++ src/main/java/com/wpaul15/scryfall/api/model/Set.java
@@ -0,0 +1,29 @@
+package com.wpaul15.scryfall.api.model;
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public record Set(
+    UUID id,
+    String code,
+    String mtgoCode,
+    int tcgplayerId,
+    String name,
+    SetType setType,
+    LocalDate releasedAt,
+    String blockCode,
+    String block,
+    String parentSetCode,
+    int cardCount,
+    int printedSize,
+    boolean digital,
+    boolean foilOnly,
+    boolean nonfoilOnly,
+    URI scryfallUri,
+    URI uri,
+    URI iconSvgUri,
+    URI searchUri
+) {
+
+}
